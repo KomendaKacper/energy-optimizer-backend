2d5decd3ac7ac81e6a518b23ebb15522bfefba93
KomendaKacper <komenda@student.agh.edu.pl>
2025-11-20 17:06:38 +0100

    Initial commit of Spring Boot backend structure

---
+++ src/test/java/com/codibly/energy_optimizer_backend/EnergyOptimizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codibly.energy_optimizer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyOptimizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
